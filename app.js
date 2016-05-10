df4534b2ca7d58da2bdde6a71230d3481df155d3
Morena Fiore <morenafiore@gmail.com>
2016-05-10 08:17:55 +0100

    Added the chooseColours controller and some data

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express'),
     path = require('path'),
-    app = express();
-    
+    app = express(),
+    chooseColours = require("./controllers/chooseColours");
+
 app.set('port', 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, '/public')));
+//app.use(express.static('public'));
 
 app.listen(app.get('port'), function () {
     console.log("Express server listening on port %s.", app.get('port'));
@@ -14,5 +16,11 @@ app.listen(app.get('port'), function () {
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
+  //res.send('hello world');
+  res.render('index', {
+    'title': 'My Site',
+    'message': 'hello'
+  });
 });
+
+app.get("/choose-colours", chooseColours.init);
--- controllers/chooseColours.js
+++ controllers/chooseColours.js
@@ -1,6 +1,43 @@
 var express = require('express'),
-    router = express.Router();
+    app = express(),
+    fs = require("fs"),
+    json;
+    //$ = require('jquery')(require("jsdom").jsdom().parentWindow);//can't use jquery on the server
 
-router.get('/choose-colours', function(req,res){
-  
-});
+function readJsonFileSync(filepath, encoding){
+
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
+
+function getConfig(file){
+
+    var filepath = __dirname + '/' + file;
+    return readJsonFileSync(filepath);
+}
+
+//assume that config.json is in application root
+
+json = getConfig('../data/colours.json');
+
+/**
+ * Respond to user click and capture the item they want to change colour for
+ */
+function respondToClick(){
+  $("#beads path").click(function(){
+    var bead = $(this);
+    console.log(bead);
+  });
+}
+
+exports.init = function(req, res){
+  var colors = json;
+
+  res.render('choose-colours', {
+    'title': 'Kodes necklaces generator',
+    'message': 'Customise your Kodes necklace'
+  });
+}
