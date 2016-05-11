4d68c7acadc461cabb341e1f5daae4a723ddea62
Morena Fiore <morenafiore@gmail.com>
2016-05-11 23:41:49 +0100

    Adding more colours and a form for the buyers details

--- controllers/confirmOrder.js
+++ controllers/confirmOrder.js
@@ -1,14 +1,48 @@
 var express = require('express'),
     app = express(),
+    json,
+		email   = require("emailjs"),
+    fs = require("fs"),
     json;
     //$ = require('jquery')(require("jsdom").jsdom().parentWindow);//can't use jquery on the server
+
+function readJsonFileSync(filepath, encoding){
+
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
+
+function getConfig(file){
+
+    var filepath = __dirname + '/' + file;
+    return readJsonFileSync(filepath);
+}
+
 /**
  * Respond to user click and capture the item they want to change colour for
  */
 exports.init = function(req, res){
-  var colours = json;
+	var json = getConfig('../data/config.json'),
+			config = json;
 
-	console.log(req.body);
+	var server  = email.server.connect({
+	   user:    config.user,
+	   password:config.password,
+	   host:    config.host,
+	   ssl:     true
+	});
+
+	//console.log(req.body);
+	// send the message and get a callback with an error or details of the message that was sent
+	server.send({
+	   text:    req.body,
+	   from:    "Kodes Order Taker <kodes@morenafiore.com>",
+	   to:      "Morena <hello@morenafiore.com>",
+	   subject: "New Kodes necklace order"
+	}, function(err, message) { console.log(err || message); });
 
   res.render('confirm', {
     'title': 'Kodes necklaces generator',
